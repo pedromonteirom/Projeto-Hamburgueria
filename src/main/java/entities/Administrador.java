202acb9383496767913c3aa03de657bec9d89a02
pedromonteirom <pedromonteiro0922@gmail.com>
2026-04-18 14:20:21 -0300

    Criação adm e func

---
+++ src/main/java/entities/Administrador.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Administrador extends Usuario {
+    
+    private String email;
+    private String telefone;
+
+    public Administrador(String nome, String login, String senha, String email, String telefone) {
+        super(nome,login,senha);
+        this.email = email;
+        this.telefone = telefone;
+    }
+    
+    public Administrador(int id, String nome, String login, String senha, String email, String telefone) {
+        super(id,nome,login,senha);
+        this.email = email;
+        this.telefone = telefone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setEmail(String email) {
+            this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+            this.telefone = telefone;
+    }
+    
+    
+            
+}
+@Override
+    public String toString(){
+        return "Administrador - ID: " + getId() + ", Nome: " + getNome() 
+                + ", Login: " + getLogin() + ", Senha: " + getSenha() 
+                + ", Telefone: " + getTelefone() + ",Email: " + getEmail();
+    }
---
+++ src/main/java/entities/Funcionario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Funcionario extends Usuario{
+       
+    private String cargo;
+    private String telefone;
+    
+        public Funcionario(String nome, String login, String senha, String cargo, String telefone) {
+        super(nome, login, senha);
+        this.cargo = cargo;
+        this.telefone = telefone;
+    }
+
+    public Funcionario(int id, String nome, String login, String senha, String cargo, String telefone) {
+        super(id, nome, login, senha);
+        this.cargo = cargo;
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
+@Override
+    public String toString(){
+        return "Funcionario - ID: " + getId() + ", Nome: " + getNome() 
+                + ", Login: " + getLogin() + ", Senha: " + getSenha() 
+                + ", Telefone: " + getTelefone() + ",Cargo: " + getCargo();
+    }
+}
