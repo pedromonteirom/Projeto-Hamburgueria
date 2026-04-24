fa317548b4b0ffd74d421b51f95ef9c344ee7645
deniobruno <deniobruno12@gmail.com>
2026-04-24 15:10:05 -0300

    Criação classe motoqueiro

--- src/main/java/entities/Motoqueiro.java
+++ src/main/java/entities/Motoqueiro.java
@@ -9,5 +9,60 @@ package entities;
  * @author Dênio Mingote
  */
 public class Motoqueiro {
-    
+
+    private static int proximoId;
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private int idRegiao;
+    private boolean disponivel;
+
+    public Motoqueiro(String nome, String telefone, int idRegiao) {
+        this.id = ++proximoId;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.idRegiao = idRegiao;
+        this.disponivel = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public int getIdRegiao() {
+        return idRegiao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean b) {
+        this.disponivel = b;
+    }
+
+    public void notificarEntregaConcluida(int idPedido) {
+        System.out.println("Entrega " + idPedido + " concluída por " + nome);
+        this.disponivel = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Motoqueiro{" +
+                "Id: " + id +
+                ", nome: '" + nome + '\'' +
+                ", telefone: '" + telefone + '\'' +
+                ", idRegiao: " + idRegiao +
+                ", disponivel: " + disponivel +
+                '}';
+    }
 }
